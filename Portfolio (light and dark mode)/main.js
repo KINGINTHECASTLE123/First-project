26259733dc34376cc95d70543f3d12b439dd7c38
jacob-bisgaard <jacbisgaard@hotmail.com>
2024-10-08 12:47:33 +0200

    Færdigt portfolie, with light/dark mode

---
+++ Portfolio (light and dark mode)/main.js
@@ -0,0 +1,23 @@
+let darkMode = localStorage.getItem('dark-mode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkMode = () => {
+    document.body.classList.add('dark-mode')
+    localStorage.setItem('dark-mode', 'active')
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-mode')
+    localStorage.setItem('dark-mode', null)
+}
+
+if (darkMode === "active") enableDarkMode()
+
+themeSwitch.addEventListener("click", () => {
+    darkMode = localStorage.getItem('dark-mode')
+    if (darkMode !== "active") {
+        enableDarkMode()
+    } else {
+        disableDarkMode()
+    }
+})
